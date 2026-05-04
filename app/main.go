88ca9db5505fe73655852274f54f5e4cda8bff93
unknown <rashed.m.alothman@gmail.com>
2026-05-04 12:05:35 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -31,7 +31,7 @@ func main() {
 			},
 		},
 		"echo": {
-			Type:        "builtin",
+			Type:        "is a shell builtin",
 			Description: "Print text to the screen",
 			Handler: func(args string) {
 				fmt.Println(args)
