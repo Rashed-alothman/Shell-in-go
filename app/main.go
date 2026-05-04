10f3bcc1af7c844fef7798bfbc3d0df2d15d88b4
unknown <rashed.m.alothman@gmail.com>
2026-05-04 10:13:33 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
-
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
+
 	fmt.Print("$ ")
+	// TODO: immplement a support for printing error message when the user input is invalid
+	//read the user input
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	//check if there is an error reading the user input
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+		log.Fatal(err)
+	}
+	//check if the user input is valid
+	fmt.Println(command[:len(command)-1] + ": command not found")
+
 }
